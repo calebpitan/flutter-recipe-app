fe0ee6f9337be7f8ad78e2e577f2a54b042049c8
Caleb Adepitan <calebpitan@outlook.com>
2022-11-07 23:57:01 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# recipes 🎉
+
+A new Flutter project.
+
+Recipes is a simpler starter/beginner flutter project where we
+learn everything is a widget, fiddle with them, give them some state
+and mutate the state like a bad genetic mutation. Sometimes we get
+good, beautiful results from the state mutations, but mostly a monster is
+born as a result of the mutations.
+
+What can I say?! 🤷🏽‍♂️
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ lib/main.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:recipes/recipe.dart';
+import 'package:recipes/recipe_detail.dart';
+
+void main() {
+  runApp(RecipeApp());
+}
+
+class RecipeApp extends StatelessWidget {
+  RecipeApp({super.key});
+
+  final ThemeData themeData = ThemeData();
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Recipe Calculator',
+      theme: themeData.copyWith(
+        colorScheme: themeData.colorScheme
+            .copyWith(primary: Colors.blueGrey, secondary: Colors.black),
+      ),
+      home: const MyHomePage(title: 'Recipe Calculator'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: SafeArea(
+        child: ListView.builder(
+            itemCount: Recipe.samples.length,
+            itemBuilder: (BuildContext context, int index) {
+              return GestureDetector(
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return RecipeDetail(recipe: Recipe.samples[index]);
+                  }));
+                },
+                child: buildRecipeCard(Recipe.samples[index]),
+              );
+            }),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Widget buildRecipeCard(Recipe recipe) {
+    return Card(
+        elevation: 2.0,
+        shape:
+        RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: <Widget>[
+              Image(image: AssetImage(recipe.imageUrl)),
+              const SizedBox(height: 14.0),
+              Text(
+                recipe.label,
+                style: const TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w700,
+                    fontFamily: 'Palatino'),
+              ),
+            ],
+          ),
+        ));
+  }
+}
